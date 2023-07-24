a0fdb88df029c18661ed67a71de6574a411cc9c4
SantosPaulAmiani <107258472+SantosPaulAmiani@users.noreply.github.com>
2023-07-24 16:35:03 +0300

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,43 +1,33 @@
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
-
     private StringBuilder number1 = new StringBuilder();
     private StringBuilder number2 = new StringBuilder();
     private char operator = ' ';
-
     private TextView textViewNumber1;
     private TextView textViewOperator;
     private TextView textViewNumber2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         textViewNumber1 = findViewById(R.id.number1);
         textViewOperator = findViewById(R.id.operator);
         textViewNumber2 = findViewById(R.id.number2);
     }
-
     public void onRefreshButtonClick(View view) {
         number1.setLength(0);
         number2.setLength(0);
         operator = ' ';
-
         textViewNumber1.setText("");
         textViewNumber2.setText("");
         textViewOperator.setText("");
     }
-
     public void onButtonClick(View view) {
         String buttonText = ((TextView) view).getText().toString();
         char firstChar = buttonText.charAt(0);
-
         if (Character.isDigit(firstChar) || buttonText.equals(".")) {
             if (operator == ' ') {
                 number1.append(buttonText);
@@ -53,13 +43,11 @@ public class MainActivity extends AppCompatActivity {
             textViewOperator.setText(Character.toString(operator));
         }
     }
-
     private void performOperation() {
         if (operator != ' ' && number1.length() > 0 && number2.length() > 0) {
             double num1 = Double.parseDouble(number1.toString());
             double num2 = Double.parseDouble(number2.toString());
             double result = 0.0;
-
             switch (operator) {
                 case '+':
                     result = num1 + num2;
@@ -84,7 +72,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                     break;
             }
-
             textViewNumber1.setText(String.valueOf(result));
             textViewNumber2.setText("");
             textViewOperator.setText("");
