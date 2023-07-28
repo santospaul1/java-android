28d700ea395166f52082e1c90c95720d9197c8b1
SantosPaulAmiani <107258472+SantosPaulAmiani@users.noreply.github.com>
2023-07-28 16:51:08 +0300

    Create sms.java

---
+++ sms.java
@@ -0,0 +1,77 @@
+package com.example.smsapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SMS_PERMISSION = 1;
+    private EditText recipientEditText;
+    private EditText messageEditText;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recipientEditText = findViewById(R.id.recipientEditText);
+        messageEditText = findViewById(R.id.messageEditText);
+        sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkSmsPermission();
+            }
+        });
+    }
+
+    private void checkSmsPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, REQUEST_SMS_PERMISSION);
+        } else {
+            sendSms();
+        }
+    }
+
+    private void sendSms() {
+        String phoneNumber = recipientEditText.getText().toString().trim();
+        String message = messageEditText.getText().toString().trim();
+
+        if (!phoneNumber.isEmpty() && !message.isEmpty()) {
+            Uri uri = Uri.parse("smsto:" + phoneNumber);
+            Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+            intent.putExtra("sms_body", message);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Please enter a phone number and message.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_SMS_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendSms();
+            } else {
+                Toast.makeText(this, "SMS permission denied.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
